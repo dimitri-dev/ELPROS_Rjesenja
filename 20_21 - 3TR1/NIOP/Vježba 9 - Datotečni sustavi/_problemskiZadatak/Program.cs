83b255ffa584b7095f1db94c17f6d6fa27612156
dimitri-dev <maric.neven03@gmail.com>
2021-02-19 10:35:02 +0100

    NIOP - excercise 9 (fix problem task)
    + Just a fix of the problem task (adding an extra output for total chars)

--- 20_21 - 3TR1/NIOP/Vježba 9 - Datotečni sustavi/_problemskiZadatak/Program.cs
+++ 20_21 - 3TR1/NIOP/Vježba 9 - Datotečni sustavi/_problemskiZadatak/Program.cs
@@ -18,13 +18,16 @@ namespace NIOP
 
             Dictionary<char, int> frequencySort = loremIpsum.GroupBy(x => x).OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Count());
 
-            // int spaceCount = (loremIpsum.Where(x => x == ' ').ToArray().Count());
+            // ALT: int spaceCount = (loremIpsum.Where(x => x == ' ').ToArray().Count());
             int spaceCount = frequencySort[' '];
 
-            Console.WriteLine($"Razmak se pojavio {spaceCount}");
-            
-            // Dictionary cleanup
+            // Dictionary cleanup of random non-letter chars.
+            // Can be removed, depending on what letter-count algorithm you want the Grouping and Ordering to handle.
+            // ALT (for totalCount): int totalCount = loremIpsum.Count(); -> for all chars (not just letters).
             frequencySort.Remove(','); frequencySort.Remove(' '); frequencySort.Remove('.');
+            int totalCount = frequencySort.Sum(x => x.Value);
+
+            Console.WriteLine($"Razmak se pojavio {spaceCount}, te je ukupno bilo {totalCount} slova.");            
             
             foreach (var k in frequencySort)
                 Console.WriteLine($"Slovo {k.Key} ponavljalo se {k.Value} puta.");
