773333ee350458d13c0d107471b7b395ed112752
dimitri-dev <maric.neven03@gmail.com>
2021-03-04 23:54:28 +0100

    NIOP - project task
    something hella boring with lots of utilizing the streamreader and streamwriter objects

---
+++ 20_21 - 3TR1/NIOP/_PZ1/_PZ1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+
+namespace _PZ1
+{
+    class NIOP
+    {
+        static void Main(string[] args)
+        {
+            Logger log = new Logger();
+            string fileText = "";
+
+            log.LogText("Inicijalizacija teksta");
+            using (var stream = new StreamReader("postojece.txt"))
+            {
+                fileText = stream.ReadToEnd();
+            }
+            log.LogText("Tekst inicijaliziran.");
+
+            log.LogText("Ispisivanje teksta sa velikim slovima.");
+            using (var stream = new StreamWriter("letters_big.txt"))
+            {
+                stream.Write(fileText.ToUpper());
+            }
+            log.LogText("Ispis izvršen.");
+
+            log.LogText("Ispivianje teksta sa malim slovima.");
+            using (var stream = new StreamWriter("letters_small.txt"))
+            {
+                stream.Write(fileText.ToLower());
+            }
+            log.LogText("Ispis izvršen");
+
+            log.LogText("Kreiranje Hello World C# datoteke...");
+            using (var stream = new StreamWriter("PozdravSvite.cs"))
+            {
+                stream.Write("using System;\n" +
+                             "using System;\n\n" +
+                             "namespace _PZ1\n" +
+                             "{\n" +
+                             "\tclass NIOP\n" +
+                             "\t{\n" +
+                             "\t\tstatic void Main(string[] args)\n" +
+                             "\t\t{\n" +
+                             "\t\t\tConsole.WriteLine(\"Hello World\");\n" +
+                             "\t\t\tConsole.ReadLine();\n" +
+                             "\t\t}\n" +
+                             "\t}\n" +
+                             "}");
+            }
+            log.LogText("Kreirana Hello World C# datoteka.");
+
+            log.LogText("Kreiranje Hello World HTML datoteke...");
+            using (var stream = new StreamWriter("PozdravSvite.html"))
+            {
+                stream.Write("<!DOCTYPE html>\n" +
+                             "<html>\n" +
+                             "\t<head>\n" +
+                             "\t\t<title>Pozdrav Svite</title>\n" +
+                             "\t</head>\n" +
+                             "\t<body>\n" +
+                             "\t\t<p style=\"text-align: center;\"> Hello world! </p>\n" +
+                             "\t</body>\n" +
+                             "</html>\n");
+            }
+            log.LogText("Kreirana Hello World HTML datoteka.");
+        }
+    }
+
+    public class Logger
+    {
+        public void LogText(string text)
+        {
+            string log = $"\n-- MAIN LOG: {DateTime.Now} - {text} --";
+
+            File.AppendAllText("log.txt", log);
+        }
+    }
+}
---
+++ 20_21 - 3TR1/NIOP/_PZ1/_PZ1/bin/Debug/netcoreapp3.1/PozdravSvite.cs
@@ -0,0 +1,14 @@
+using System;
+using System;
+
+namespace _PZ1
+{
+	class NIOP
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World");
+			Console.ReadLine();
+		}
+	}
+}
